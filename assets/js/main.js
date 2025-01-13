dad6256264067aa38c115c21da8c5e8043cef198
zaenal-abidinn <zabid270523@gmail.com>
2025-01-13 20:18:15 +0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
+AOS.init({
+  duration: 1000,
+  easing: 'ease-in-out',
+  once: false
+})
 
+// Script untuk membuka dan menutup menu mobile
+const mobileMenuButton = document.getElementById('mobile-menu-button')
+const mobileMenu = document.getElementById('mobile-menu')
+
+mobileMenuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden')
+  // Animasi untuk efek menu
+  if (mobileMenu.classList.contains('hidden')) {
+    mobileMenu.classList.remove('opacity-100')
+    mobileMenu.classList.add('opacity-0')
+  } else {
+    mobileMenu.classList.remove('opacity-0')
+    mobileMenu.classList.add('opacity-100')
+  }
+})
